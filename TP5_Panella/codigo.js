8e7711d33910b17fd67ea6cb524c4828ee549ca1
Lautaro-Panella <raull.panella@alumnos.frm.utn.edu.ar>
2021-01-23 15:51:44 -0300

    Subo proyectos JavaScript - TSP.

---
+++ TP5_Panella/codigo.js
@@ -0,0 +1,2 @@
+var mensaje1 = alert("Hola mundo!"); // Lanzo una alerta con el texto "Hola mundo!".
+var mensaje2 = alert("Soy el primer script"); // Lanzo una alerta con el texto "Soy el primer script".
